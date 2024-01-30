ab167de8ae3aac3e5f47c81b8a0c41ed83d69976
OzymandiasBrando <127154170+OzymandiasBrando@users.noreply.github.com>
2024-01-30 17:32:32 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,26 +1,20 @@
-// Seleciona o botão
-const botaoCopiar = document.getElementById('botao-copiar');
+const botoesCopiar = document.querySelectorAll('[id^="botao-copiar"]');
 
-// Adiciona um ouvinte de evento de clique ao botão
-botaoCopiar.addEventListener('click', () => {
-    // Obtém o número a ser copiado do atributo de dados do botão
-    const numeroParaCopiar = botaoCopiar.getAttribute('data-numero');
+botoesCopiar.forEach(botao => {
+    botao.addEventListener('click', () => {
+        
+        const numeroParaCopiar = botao.getAttribute('data-numero');
 
-    // Cria um elemento temporário para armazenar o número a ser copiado
-    const elementoTemporario = document.createElement('textarea');
-    
-    // Define o valor do texto a ser copiado como o número do atributo de dados
-    elementoTemporario.value = numeroParaCopiar;
+        const elementoTemporario = document.createElement('textarea');
+        
+        elementoTemporario.value = numeroParaCopiar;
 
-    // Adiciona o elemento temporário ao documento
-    document.body.appendChild(elementoTemporario);
+        document.body.appendChild(elementoTemporario);
 
-    // Seleciona o texto dentro do elemento temporário
-    elementoTemporario.select();
+        elementoTemporario.select();
+        
+        document.execCommand('copy');
 
-    // Tenta copiar o texto selecionado para a área de transferência do navegador
-    document.execCommand('copy');
-
-    // Remove o elemento temporário do documento
-    document.body.removeChild(elementoTemporario);
+        document.body.removeChild(elementoTemporario);
+    });
 });
